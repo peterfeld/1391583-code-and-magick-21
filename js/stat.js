e2bbcac34074124647d4d2587cbbbf27e4d15ef3
peterfeld <89661123000@mail.ru>
2020-10-01 00:28:10 +0300

    Добавляет статистику

---
+++ js/stat.js
@@ -0,0 +1,66 @@
+'use strict';
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 135;
+const CLOUD_Y = 265;
+const BAR_WIDTH = 40;
+const BAR_MAX_HEIGHT = 150;
+const GAP = 50;
+const TEXT_WIDTH = 50;
+const FONT_GAP = 15;
+
+let Font = {
+  SIZE: `16px`,
+  FAMILY: `PT Mono`
+};
+
+let renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+let getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function(ctx, names, times) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = `${Font.SIZE} ${Font.FAMILY}`;
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+  let maxTime = getMaxElement(times);
+
+  let SaturationRandom = () => {
+    return "hsl(" + '235' + ',' + (100*Math.random()) + '%,' + '34%)';
+  }
+
+  for (let i=0; i<names.length; i++) {
+    let barHeight = BAR_MAX_HEIGHT* times[i] / maxTime;
+    ctx.fillText(Math.round(times[i]), CLOUD_X+(BAR_WIDTH+GAP)* i, CLOUD_Y- FONT_GAP*2-barHeight);
+
+      if (names[i] == "Вы") {
+        ctx.fillStyle = "rgba(255, 0, 0, 1)";
+      }
+
+      else {
+        ctx.fillStyle = SaturationRandom();
+      }
+
+    ctx.fillRect(CLOUD_X+(BAR_WIDTH+GAP)* i, CLOUD_Y - FONT_GAP - barHeight, BAR_WIDTH, barHeight);
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[i], CLOUD_X+(BAR_WIDTH+GAP)* i, CLOUD_Y);
+  }
+};
+
